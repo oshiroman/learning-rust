5d17f4251e8d20a470818349550b3db7cee3458f
oshiroman <r2.valna@gmail.com>
2024-04-05 18:15:52 +0900

    feat: add gen_range

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -10,5 +15,5 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    println!("You guessed: {guess}");
 }

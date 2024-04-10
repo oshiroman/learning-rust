f11a19eb2a97619e0609b52baa4f238d0fd1992c
Tatsuya Oshiro <r2.valna@gmail.com>
2024-04-11 02:21:33 +0900

    feat: 3.2 data types

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,15 +1,34 @@
 fn main() {
-    let x = 5;
-    let x = x + 1;
+    let guess = "614".parse::<i32>().unwrap();
 
-    println!("The value of x is: {x}");
+    let x = 3.0;
+    let y: f32 = 2.7182818;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    // addition
+    let sum = 2 + 3;
+    println!("[sum] {sum}");
 
-    let x = x + 2;
+    // subtraction
+    let diff = 1.732 - 2.236;
+    println!("[diff] {diff}");
 
-    println!("The value of x is: {x}");
+    // multiplication
+    let product = 12345679 * 23;
+    println!("[product] {product}");
+
+    // division
+    let quotient = 58.0 / 173.1;
+    let truncated = -2401 / 2443;
+    println!("[quotient] {quotient}");
+    println!("[truncated] {truncated}");
+
+    // remainder
+    let remainder = 341 % 17;
+    println!("[remainder] {remainder}");
+}
+
+#[test]
+fn test() {
+    let guess: u32 = "614".parse().expect("Not a number!");
+    assert_eq!(guess, 614)
 }
